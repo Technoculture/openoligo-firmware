a95e06eb21173b7b44e5bf29ff799ae4d829ba25
Satyam Tiwary <satyam@technoculture.io>
2023-06-20 20:15:18 +0530

    init

---
+++ openoligo/main.py
@@ -0,0 +1,9 @@
+"""
+This module contains the main entry point for the openoligo application.
+"""
+
+def main():
+    """
+    This function is the main entry point of the program.
+    """
+    print("Hello World!")
