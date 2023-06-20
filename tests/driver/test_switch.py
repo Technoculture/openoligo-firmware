25540138c9abf711b80ef1d1cbd45fe6b9c1279f
Satyam Tiwary <satyam@technoculture.io>
2023-06-21 01:57:13 +0530

    init

--- tests/driver/test_switch.py
+++ tests/driver/test_switch.py
@@ -1,4 +1,5 @@
 from openoligo.driver.switch import SimulatedSwitch, periodic_toggle
+from openoligo.driver.types import SwitchingError
 import pytest
 from unittest.mock import Mock, patch
 
@@ -14,20 +15,6 @@ def test_simulated_switch():
     s1.set(False)
     assert s1.value == False
 
-
-#@pytest.mark.asyncio
-#async def test_periodic_toggle():
-#    s1 = SimulatedSwitch(pin=1, name="test_switch")
-#
-#    # run the function for 5 toggles
-#    await periodic_toggle(switch=s1, interval=0.01, loop_forever=False, count=5)
-#    assert s1.switch_count == 5
-#
-#    # await periodic_toggle(switch=s1, interval=0.01, loop_forever=False, count=0)
-#    pytest.raises(AssertionError, periodic_toggle, s1, 0, False, 1)
-#    pytest.raises(AssertionError, periodic_toggle, s1, 0.01, False, 0)
-#    pytest.raises(AssertionError, periodic_toggle, s1, 0.01, False, -1)
-#    pytest.raises(AssertionError, periodic_toggle, s1, -1, False, 1)
 
 @pytest.mark.asyncio
 async def test_periodic_toggle():
@@ -50,10 +37,8 @@ async def test_periodic_toggle():
 @pytest.mark.asyncio
 async def test_periodic_toggle_exception_handling():
     s1 = Mock()  # create a mock switch
-    s1.toggle.side_effect = Exception("test")  # raise exception when toggle is called
+    s1.toggle.side_effect = SwitchingError("test")  # raise exception when toggle is called
 
     with patch('logging.error') as mock_log:  # patch logging to check if it logs the exception
         await periodic_toggle(switch=s1, interval=0.01, loop_forever=False, count=1)
         mock_log.assert_called()  # assert that the error was logged
-
-
